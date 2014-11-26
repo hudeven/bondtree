14acf44a55a8a67c1ddf8ff0a3bd0a6a8b5d5127
StevenLiu <xyliu.365@gmail.com>
2014-11-25 20:15:19 -0500

    no changes

--- src/debug_ndTree.sh
+++ src/debug_ndTree.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo gdb --args ndTree -idxfile ../data/index_real -dscdim 16 -load_file ../data/data_random -bqfile ../data/query_random -skip 0 -newtree 1 -aux_file ../data/cancer/kmer/all.kmer.desc -record_file ../data/record
+sudo gdb --args ndTree --dimension 16 --index ../data/index_real --data ../data/cancer/kmer/all.kmer --boxquery ../data/cancer/query/boxquery --skip 0 --newtree 1 --aux ../data/cancer/kmer/all.kmer.desc --record ../data/record --querydim 6
